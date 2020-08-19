fee42b034921dcaba506b111639d9ce61482ebe9
jerajanip <69919213+jerajanip@users.noreply.github.com>
2020-08-19 14:29:35 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+*Lab1
+**********************************************************************************
+*Preksha Jerajni
+*date:August8th2020
+*CMSC255 section002
+*************************************************************************************/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Preksha Jerajani");
+		System.out.println("Lab 1");
+		System.out.println("Download JAVA and run program");
+		System.out.println("08/19/2020");
+		System.out.println("CMSC255 and section002");
+		System.out.println();
+		}
+	}
+		
